27ebd5e7fc43a5a00994ab2c843be28a672d198e
Radhika Mittal <mitt0047@algonquinlive.com>
2025-01-20 20:30:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+//Task1
+let a = 3;
+let b = 5;
+let c;
+
+let output1= "let a = 3;\n" +
+  "let b = 5;\n" +
+  "let c;\n" +
+  "--------------------\n" +
+  "a + b = " + (a + b) + "\n" +
+  "a - b = " + (a - b) + "\n" +
+  "a * b = " + (a * b) + "\n" +
+  "a / b = " + (a / b) + "\n" +
+  "a % b = " + (a % b) + "\n" +
+  "a += b = " + (a += b) + "\n" +
+  "a -= b = " + (a -= b) + "\n" +
+  "a *= b = " + (a *= b) + "\n" +
+  "a /= b = " + (a /= b) + "\n" +
+  "a %= b = " + (a %= b) + "\n" +
+  "a == b = " + (a == b) + "\n" +
+  "a != b = " + (a != b) + "\n" +
+  "a > b = " + (a > b) + "\n" +
+  "a < b = " + (a < b) + "\n" +
+  "!a && !c = " + (!a && !c) + "\n" +
+  "!a || !c = " + (!a || !c);
+
+  console.log(output1)
+
+//Task2
+let first_name= "Radhika";
+let last_name= "Mittal";
+let email= "mitt0047@algonquinlive.com";
+let output;
+console.log(`My name is ${first_name} ${last_name}. You can contact me at ${email}.`);
+output= "Received";
+console.log(output);
+
 
